a5280a00b2ecd840c18b775aea8339fb32382550
sde-alva <sde-alva@student.42sp.org.br>
2022-03-21 15:58:45 -0300

    improvement on number validation

--- philo/ft_checker_args.c
+++ philo/ft_checker_args.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 11:39:46 by sde-alva          #+#    #+#             */
-/*   Updated: 2022/03/18 11:39:50 by sde-alva         ###   ########.fr       */
+/*   Updated: 2022/03/21 15:56:14 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 static int	ft_all_char_valid(char **argv);
 static int	ft_all_int_valid(char **argv);
+static int	ft_number_converter_abs_dif(char *charnum, int number);
 
 int	ft_check_args(char **argv)
 {
@@ -38,7 +39,9 @@ static int	ft_all_char_valid(char **argv)
 		j = 0;
 		while (is_valid && argv[i][j])
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+			if (j == 0 && argv[i][0] == '+')
+				j++;
+			if (argv[i][j] && (argv[i][j] < '0' || argv[i][j] > '9'))
 				is_valid = 0;
 			j++;
 		}
@@ -58,9 +61,29 @@ static int	ft_all_int_valid(char **argv)
 	while (argv[i])
 	{
 		number = ft_atoi(argv[i]);
-		if (number < 0)
+		if (number < 0 || ft_number_converter_abs_dif(argv[i], number) > 2)
 			is_valid = 0;
 		i++;
 	}
 	return (is_valid);
+}
+
+static int	ft_number_converter_abs_dif(char *charnum, int number)
+{
+	int	charnum_size;
+	int	number_size;
+
+	charnum_size = ft_strlen(charnum);
+	number_size = 0;
+	if (number == 0)
+		number_size = 1;
+	while (number > 0)
+	{
+		number_size++;
+		number /= 10;
+	}
+	if (number_size > charnum_size)
+		return (number_size - charnum_size);
+	else
+		return (charnum_size - number_size);
 }

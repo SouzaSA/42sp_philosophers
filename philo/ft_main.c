796d4f004af225aa14b9325aa791a99f3d68a792
sde-alva <sde-alva@student.42sp.org.br>
2022-03-28 15:54:27 -0300

    changed error message on invalid parameters

--- philo/ft_main.c
+++ philo/ft_main.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 11:40:17 by sde-alva          #+#    #+#             */
-/*   Updated: 2022/03/20 19:47:00 by sde-alva         ###   ########.fr       */
+/*   Updated: 2022/03/28 15:46:54 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(int argc, char **argv)
 	{
 		if (ft_load_table(argc, argv, &table))
 		{
-			write(2, "error: can't load variables\n", 29);
+			write(2, "error: invalid variables\n", 26);
 			return (1);
 		}
 		ft_philosophers(&table);

61eb92f5d49b7ff9befb9cb88f2c333138e25cc5
sde-alva <sde-alva@student.42sp.org.br>
2022-04-03 16:26:53 +0200

    change behavior on only one philo

--- philo_bonus/ft_dinner_bonus.c
+++ philo_bonus/ft_dinner_bonus.c
@@ -6,13 +6,14 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 18:49:15 by sde-alva          #+#    #+#             */
-/*   Updated: 2022/04/03 00:35:34 by sde-alva         ###   ########.fr       */
+/*   Updated: 2022/04/03 16:23:34 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_philo_bonus.h"
 
 static void	*ft_reaper(void *arg);
+static void ft_alone_actions(t_philo *philo);
 static void	ft_philo_actions(t_philo *philo);
 static void	ft_meal_checker(t_philo *philo);
 
@@ -25,7 +26,10 @@ void	ft_dinner(t_philo *philo)
 		ft_clean_exit(philo);
 		exit(1);
 	}
-	ft_philo_actions(philo);
+	if (philo->stats->num_philo == 1)
+		ft_alone_actions(philo);
+	else
+		ft_philo_actions(philo);
 	ft_clean_exit(philo);
 }
 
@@ -47,6 +51,14 @@ static void	*ft_reaper(void *philo_void)
 		}
 	}
 	return (NULL);
+}
+
+static void ft_alone_actions(t_philo *philo)
+{
+	sem_wait(philo->semaphores->sem_fork);
+	ft_put_msg("has taken a fork", philo, ALIVE);
+	sem_wait(philo->semaphores->sem_fork);
+	ft_msleep(philo->stats->time_to_die + 1);
 }
 
 static void	ft_philo_actions(t_philo *philo)
